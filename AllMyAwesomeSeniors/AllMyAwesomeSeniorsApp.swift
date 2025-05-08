9aa8046318bef6e9cb62a6787ae5acfc2724afd2
Derek Fitzer <franzderek@gmail.com>
2025-05-08 13:46:30 -0400

    Initial Commit

---
+++ AllMyAwesomeSeniors/AllMyAwesomeSeniorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllMyAwesomeSeniorsApp.swift
+//  AllMyAwesomeSeniors
+//
+//  Created by Derek Fitzer on 5/8/25.
+//
+
+import SwiftUI
+
+@main
+struct AllMyAwesomeSeniorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
