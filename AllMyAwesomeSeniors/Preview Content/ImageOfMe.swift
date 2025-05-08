839b69ab87449b12dfa404cf580bc2cd5494876d
Caleb <110161@swcsd.us>
2025-05-08 14:07:33 -0400

    message

---
+++ AllMyAwesomeSeniors/Preview Content/ImageOfMe.swift
@@ -0,0 +1,18 @@
+//
+//  ImageOfMe.swift
+//  AllMyAwesomeSeniors
+//
+//  Created by Caleb Eric Bernholtz on 5/8/25.
+//
+
+import SwiftUI
+
+struct ImageOfMe: View {
+    var body: some View {
+        Image("images")
+    }
+}
+
+#Preview {
+    ImageOfMe()
+}
